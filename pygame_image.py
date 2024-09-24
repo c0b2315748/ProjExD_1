e501d518a4555934d78e4100080344e9a013b71e
KosugeRyo <c0b2315748@edu.teu.ac.jp>
2024-09-24 15:46:05 +0900

    rensyu6

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = -(tmr%800)
+        screen.blit(bg_img, [x, 0])
         screen.blit(kk_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
