5e95304fd2dfc7da5a2af616f060b9983538d34d
KosugeRyo <c0b2315748@edu.teu.ac.jp>
2024-09-24 16:37:57 +0900

    ensyu1

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        if key_lst:
+            kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1)) # こうかとんの縦座標を―1する
         if key_lst[pg.K_DOWN]:
@@ -29,6 +31,7 @@ def main():
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((1, 0))
+        
             
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
