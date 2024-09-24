0ce906330010b80b3063c10d65512db5edd9b3e7
KosugeRyo <c0b2315748@edu.teu.ac.jp>
2024-09-24 16:24:26 +0900

    rensyu8

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,29 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_rct = kk_img.get_rect() #8-1
+    kk_rct.center = 300, 200 #8-2
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1)) # こうかとんの縦座標を―1する
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+            
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(kk_img, [300, 200]) #4
+        screen.blit(kk_img, kk_rct) #4  #8-5
         pg.display.update()
         tmr += 1        
         clock.tick(200) #5
